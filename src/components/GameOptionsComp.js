713029bb2cfd5742761b753683701d45be281051
KarinaRamirezDS <karinaramirezds@gmail.com>
2022-07-02 21:31:19 -0500

    "first commit"

---
+++ src/components/GameOptionsComp.js
@@ -0,0 +1,48 @@
+import { Row, Col, Button } from "react-bootstrap";
+
+const GameOptionsComp = ({ disks, setDisks, reset, solve }) => {
+  return (
+    <Row className="componentes">
+      <Col>
+        <div>
+          {" "}
+          <span>Discos: {disks}</span>{" "}
+        </div>
+
+        <Button
+          variant="outline-secondary"
+          onClick={() => {
+            setDisks((disks += 1));
+          }}
+        >
+          +
+        </Button>
+
+        <Button
+          variant="outline-secondary"
+          onClick={() => {
+            if (disks >= 2) {
+              setDisks((disks -= 1));
+            }
+          }}
+        >
+          -
+        </Button>
+      </Col>
+      <Col>
+        <br></br>
+        <Button variant="outline-secondary" onClick={() => reset()}>
+          Reiniciar
+        </Button>
+      </Col>
+      <br></br>
+      <Col>
+        <Button variant="outline-secondary" onClick={() => solve()}>
+          Resolver
+        </Button>
+      </Col>
+    </Row>
+  );
+};
+
+export default GameOptionsComp;
